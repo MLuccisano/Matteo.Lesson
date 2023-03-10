712236caaa884a61cd9610d6c7dbce77e9a8f5c6
Matteo <105592696+Howard3798@users.noreply.github.com>
2023-03-10 12:47:27 +0100

    Prova

---
+++ Le classi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Le_classi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
